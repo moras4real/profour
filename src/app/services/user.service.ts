18c375ab7cc7b83a22839f0adb976837bd0313dc
Ojo Morakinyo Samuel <arayosam@gmail.com>
2023-10-10 11:26:11 +0100

    saved

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -8,14 +8,14 @@ export class UserService {
 
   constructor(public http : HttpClient ) { }
 
-  public getFrontend(){
-    return this.http.get("https://prophp.infinityfreeapp.com/dashboard.php");
-   }
+  // public getFrontend(){
+  //   return this.http.get("https://prophp.infinityfreeapp.com/dashboard.php");
+  //  }
 
   
-  //  public getFrontend(){
-  //   return this.http.get("http://localhost/prophp/dashboard.php");
-  //  }
+   public getFrontend(){
+    return this.http.get("http://localhost/prophp/dashboard.php");
+   }
    
   // public postFrontend(obj:any){
   //    return this.http.post("http://localhost/firstfolder/dashboard.php",obj);
